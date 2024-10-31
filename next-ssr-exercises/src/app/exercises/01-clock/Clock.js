fb2f8cdc8a3c23936a7606ff5390584660e2ece6
KaitlynParsons <kaitie.98@hotmail.com>
2024-10-31 21:00:10 +1000

    joy-of-react: Module 6, SSR exercises

---
+++ next-ssr-exercises/src/app/exercises/01-clock/Clock.js
@@ -0,0 +1,24 @@
+'use client';
+import React from 'react';
+import format from 'date-fns/format';
+import Spinner from '../../../components/Spinner';
+
+function Clock() {
+  const [time, setTime] = React.useState();
+
+  React.useEffect(() => {
+    const intervalId = window.setInterval(() => {
+      setTime(new Date());
+    }, 50);
+
+    return () => {
+      window.clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <p className="clock">{time ? format(time, 'hh:mm:ss.S a') : <Spinner />}</p>
+  );
+}
+
+export default Clock;
---
+++ next-ssr-exercises/src/app/exercises/02-checkout/page.js
@@ -0,0 +1,70 @@
+'use client';
+import React, { useEffect } from 'react';
+
+import DATA from './data';
+import reducer from './reducer';
+import StoreItem from './StoreItem';
+import CheckoutFlow from './CheckoutFlow';
+import './styles.css';
+
+function CheckoutExercise() {
+  const [items, dispatch] = React.useReducer(reducer, null);
+
+  useEffect(() => {
+    const savedItems =
+      window.localStorage.getItem('cart-items');
+      dispatch({
+        type: 'initialize',
+        items:
+          savedItems === null
+            ? []
+            : JSON.parse(savedItems),
+      });
+  }, []);
+
+  // Store items in local storage when the items change
+  useEffect(() => {
+    if (items !== null) {
+      window.localStorage.setItem(
+        'cart-items',
+        JSON.stringify(items)
+      );
+    }
+  }, [items]);
+
+  return (
+    <>
+      <h1>Neighborhood Shop</h1>
+
+      <main>
+        <div className='items'>
+          {DATA.map((item) => (
+            <StoreItem
+              key={item.id}
+              item={item}
+              handleAddToCart={(item) => {
+                dispatch({
+                  type: 'add-item',
+                  item,
+                });
+              }}
+            />
+          ))}
+        </div>
+
+        <CheckoutFlow
+          items={items}
+          taxRate={0.15}
+          handleDeleteItem={(item) =>
+            dispatch({
+              type: 'delete-item',
+              item,
+            })
+          }
+        />
+      </main>
+    </>
+  );
+}
+
+export default CheckoutExercise;
