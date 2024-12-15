3ccf6518926a2fdfbb9ea591b98adb31b8917a38
KaitlynParsons <kaitie.98@hotmail.com>
2024-12-15 11:58:12 +1000

    joy-of-react: project-blog, exercise 4

--- project-blog/src/app/[postSlug]/page.js
+++ project-blog/src/app/[postSlug]/page.js
@@ -4,6 +4,7 @@ import { MDXRemote } from 'next-mdx-remote/rsc';
 import { loadBlogPost } from '@/helpers/file-helpers';
 import { BLOG_TITLE } from '@/constants';
 import BlogHero from '@/components/BlogHero';
+import CodeSnippet from '@/components/CodeSnippet';
 
 import styles from './postSlug.module.css';
 
@@ -18,6 +19,13 @@ export async function generateMetadata({ params }) {
   };
 }
 
+const components = {
+  pre: (props) => (
+    <CodeSnippet {...props} />
+  ),
+};
+
+
 async function BlogPost({params}) {
   const {frontmatter, content} = await loadBlogPost(params.postSlug);
 
@@ -28,7 +36,7 @@ async function BlogPost({params}) {
         publishedOn={new Date(frontmatter.publishedOn)}
       />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote source={content} components={components} />
       </div>
     </article>
   );
